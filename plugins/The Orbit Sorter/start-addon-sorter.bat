f8bc21b57db8fb2fa16488e3c8b0154d8117daa4
lucaskerim123 <lucas.kerim@gmail.com>
2026-07-12 13:01:39 +0000

    33

--- plugins/The Orbit Sorter/start-addon-sorter.bat
+++
@@ -1,6 +0,0 @@
-﻿@echo off
-set "SORTER_DIR=%~dp0"
-cd /d "%SORTER_DIR%"
-start "OrbitFS Sorter" cmd /k "node server.js"
-echo OrbitFS Sorter starting on http://localhost:4055
-pause
--- plugins/The Orbit Sorter/stop-addon-sorter.bat
+++
@@ -1,4 +0,0 @@
-﻿@echo off
-set "SORTER_DIR=%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -Command "$dir = [IO.Path]::GetFullPath('%SORTER_DIR%'); $needle = [IO.Path]::Combine($dir, 'server.js'); $p = Get-CimInstance Win32_Process | Where-Object { $_.CommandLine -like ('*' + $needle + '*') -or $_.CommandLine -like '*orbitfs-sorter*server.js*' }; if ($p) { $p | ForEach-Object { Stop-Process -Id $_.ProcessId -Force; Write-Host ('Stopped OrbitFS Sorter PID ' + $_.ProcessId) } } else { Write-Host 'OrbitFS Sorter is not running.' }"
-pause
